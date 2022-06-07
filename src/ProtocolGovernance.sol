9d40ee760a549f11807b8138bb4f8b2c1b35b978
cube <cubetv228@gmail.com>
2022-06-08 00:49:01 +0300

    added constants

--- src/ProtocolGovernance.sol
+++ src/ProtocolGovernance.sol
@@ -5,5 +5,19 @@ pragma solidity ^0.8.13;
 import "./interfaces/IProtocolGovernance.sol";
 
 contract ProtocolGovernance is IProtocolGovernance {
-    
+    address private admin;
+    address private validator;
+    uint256 constant DENOMINATOR = 10 ** 9;
+    uint256 private governanceDelay;
+    uint256 private stabilisationFeeD;
+    uint256 private liquidationFeeD;
+    uint256 private liquidationPremiumD;
+    uint256 private liguidationThresholds;
+    mapping(address => bool) private whitelistedPools;
+    uint256 private MAX_DEBT_AMOUNT;
+    uint256 private MIN_COLLATERAL_AMOUNT;
+
+    constructor() {
+
+    }
 }
--- src/interfaces/IProtocolGovernance.sol
+++ src/interfaces/IProtocolGovernance.sol
@@ -3,5 +3,5 @@ pragma solidity ^0.8.13;
 
 
 contract IProtocolGovernance {
-
+    
 }
