7ff27fbc6a9f2248db364fa5696d1bc0509e5405
Alexey Saplin <40490358+AlexSaplin@users.noreply.github.com>
2022-06-15 15:57:16 +0400

    oracle fixed

--- src/oracles/ChainlinkOracle.sol
+++ src/oracles/ChainlinkOracle.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: BSL-1.1
 pragma solidity 0.8.13;
-
 import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 import "../interfaces/external/chainlink/IAggregatorV3.sol";
@@ -9,15 +8,15 @@ import "../libraries/ExceptionsLibrary.sol";
 import "../libraries/CommonLibrary.sol";
 import "../utils/DefaultAccessControl.sol";
 import "../interfaces/oracles/IOracle.sol";
-
 /// @notice Contract for getting chainlink data
 contract ChainlinkOracle is IOracle, DefaultAccessControl {
     using EnumerableSet for EnumerableSet.AddressSet;
 
+    int256 constant DECIMALS = 18;
+
     mapping(address => address) public oraclesIndex;
     mapping(address => int256) public decimalsIndex;
     EnumerableSet.AddressSet private _tokens;
-
     constructor(
         address[] memory tokens,
         address[] memory oracles,
@@ -31,37 +30,30 @@ contract ChainlinkOracle is IOracle, DefaultAccessControl {
     function hasOracle(address token) external view returns (bool) {
         return _tokens.contains(token);
     }
-
     function supportedTokens() external view returns (address[] memory) {
         return _tokens.values();
     }
 
     /// @inheritdoc IOracle
-    function price(address token0, address token1) external view returns (uint256 priceX96) {
+    function price(address token) external view returns (uint256 priceX96) {
         priceX96 = 0;
-        IAggregatorV3 chainlinkOracle0 = IAggregatorV3(oraclesIndex[token0]);
-        IAggregatorV3 chainlinkOracle1 = IAggregatorV3(oraclesIndex[token1]);
-        if ((address(chainlinkOracle0) == address(0)) || (address(chainlinkOracle1) == address(0))) {
+        IAggregatorV3 chainlinkOracle = IAggregatorV3(oraclesIndex[token]);
+        if (address(chainlinkOracle) == address(0)) {
             return priceX96;
         }
         uint256 price0;
-        uint256 price1;
+        uint256 price1 = 1;
         bool success;
         (success, price0) = _queryChainlinkOracle(chainlinkOracle0);
         if (!success) {
             return priceX96;
         }
-        (success, price1) = _queryChainlinkOracle(chainlinkOracle1);
-        if (!success) {
-            return priceX96;
-        }
 
-        int256 decimals0 = decimalsIndex[token0];
-        int256 decimals1 = decimalsIndex[token1];
-        if (decimals1 > decimals0) {
-            price1 *= 10**(uint256(decimals1 - decimals0));
+        int256 decimals0 = decimalsIndex[token];
+        if (DECIMALS > decimals0) {
+            price1 *= 10**(uint256(DECIMALS - decimals0));
         } else if (decimals0 > decimals1) {
-            price0 *= 10**(uint256(decimals0 - decimals1));
+            price0 *= 10**(uint256(decimals0 - DECIMALS));
         }
         priceX96 = FullMath.mulDiv(price0, CommonLibrary.Q96, price1);
     }
