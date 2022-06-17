b9ced6fc7873f393537419d665b21034a8025753
Alexey Saplin <40490358+AlexSaplin@users.noreply.github.com>
2022-06-17 20:34:11 +0400

    compatible state

--- src/Vault.sol
+++ src/Vault.sol
@@ -4,12 +4,12 @@ pragma solidity 0.8.13;
 import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 import "@openzeppelin/contracts/mocks/EnumerableSetMock.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./interfaces/external/univ3/INonfungiblePositionManager.sol";
 import "./interfaces/external/univ3/IUniswapV3Pool.sol";
 import "./interfaces/oracles/IOracle.sol";
 import "./utils/DefaultAccessControl.sol";
 import "./interfaces/IProtocolGovernance.sol";
+import "./interfaces/IMUSD.sol";
 import "./interfaces/external/univ3/IUniswapV3Factory.sol";
 import "./libraries/ExceptionsLibrary.sol";
 import "./libraries/CommonLibrary.sol";
@@ -17,7 +17,6 @@ import "./libraries/external/LiquidityAmounts.sol";
 import "./libraries/external/FullMath.sol";
 import "./libraries/external/TickMath.sol";
 
-// todo: add stabilisation fee
 
 contract Vault is DefaultAccessControl {
     using EnumerableSet for EnumerableSet.AddressSet;
@@ -40,24 +39,48 @@ contract Vault is DefaultAccessControl {
         uint256 vaultId;
     }
 
-    bool public isPaused = false;
-    bool public isPrivate = false;
     INonfungiblePositionManager public immutable positionManager;
-    IOracle public oracle;
     IUniswapV3Factory public immutable factory;
     IProtocolGovernance public protocolGovernance;
+    IOracle public oracle;
+    IMUSD public immutable token;
+    address public immutable treasury;
+
+    bool public isPaused = false;
+    bool public isPrivate = true;
+
     EnumerableSet.AddressSet private _depositorsAllowlist;
-    IERC20 public immutable token;
-    mapping(address => EnumerableSet.UintSet) public ownedVaults;
-    mapping(uint256 => EnumerableSet.UintSet) public vaultNfts;
+    mapping(address => EnumerableSet.UintSet) private _ownedVaults;
+    mapping(uint256 => EnumerableSet.UintSet) private _vaultNfts;
     mapping(uint256 => address) public vaultOwners;
     mapping(uint256 => uint256) public debt;
     mapping(uint256 => PositionInfo) private _positionInfo;
-    address public immutable treasury;
+
     uint256 vaultCount = 0;
 
-    function ownedVaults(address target) external view returns (uint256[] memory) {
-        return ownedVaults[target].values();
+    constructor(
+        address admin,
+        INonfungiblePositionManager positionManager_,
+        IUniswapV3Factory factory_,
+        IProtocolGovernance protocolGovernance_,
+        IOracle oracle_,
+        IMUSD token_,
+        address treasury_
+    ) DefaultAccessControl(admin) {
+        positionManager = positionManager_;
+        factory = factory_;
+        protocolGovernance = protocolGovernance_;
+        oracle = oracle_;
+        token = token_;
+        treasury = treasury_;
+    }
+
+    function ownedVaultsByAddress(address target) external view returns (uint256[] memory) {
+        return _ownedVaults[target].values();
+    }
+
+    function vaultNftsById(uint256 vaultId) external view returns (uint256[] memory) {
+        return _vaultNfts[vaultId].values();
     }
 
     function depositorsAllowlist() external view returns (address[] memory) {
@@ -65,16 +88,24 @@ contract Vault is DefaultAccessControl {
     }
 
     function openVault() external returns (uint256 vaultId) {
-        require(!isPrivate || _depositorsAllowlist.contains(msg.sender), ExceptionsLibrary.ALLOWLIST);
+        if (isPrivate && !_depositorsAllowlist.contains(msg.sender)) {
+            revert ExceptionsLibrary.AllowList();
+        }
+
         ++vaultCount;
-        ownedVaults[msg.sender].add(vaultCount);
+        _ownedVaults[msg.sender].add(vaultCount);
         vaultOwners[vaultCount] = msg.sender;
+
         return vaultCount;
     }
 
     function closeVault(uint256 vaultId) external {
-        require(vaultOwner[vaultId] == msg.sender, ExceptionsLibrary.FORBIDDEN);
-        require(debt[vaultId] == 0, ExceptionsLibrary.UNPAID_DEBT);
+        _requireVaultOwner(vaultId);
+
+        if (debt[vaultId] != 0) {
+            revert ExceptionsLibrary.UnpaidDebt();
+        }
+
         _closeVault(vaultId, msg.sender, msg.sender);
     }
 
@@ -83,18 +114,19 @@ contract Vault is DefaultAccessControl {
         address vaultOwner,
         address nftsRecipient
     ) internal {
-        uint256[] memory nfts = vaultNfts[];
+        uint256[] memory nfts = _vaultNfts[vaultId].values();
 
-        for (uint256 i = 0; i < nfts.length(); ++i) {
+        for (uint256 i = 0; i < _vaultNfts[vaultId].length(); ++i) {
             // todo: add limits support
             delete _positionInfo[nfts[i]];
+
             positionManager.safeTransferFrom(address(this), nftsRecipient, nfts[i]);
         }
 
+        _ownedVaults[vaultOwner].remove(vaultId);
         delete debt[vaultId];
-        ownedVaults[vaultOwner].remove(vaultId);
         delete vaultOwners[vaultId];
-        delete vaultNfts[vaultId];
+        delete _vaultNfts[vaultId];
     }
 
     function addDepositorsToAllowlist(address[] calldata depositors) external {
@@ -112,69 +144,86 @@ contract Vault is DefaultAccessControl {
     }
 
     function depositCollateral(uint256 vaultId, uint256 nft) external {
-        require(!isPrivate || _depositorsAllowlist.contains(msg.sender), ExceptionsLibrary.ALLOWLIST);
-        require(vaultOwner[vaultId] == msg.sender, ExceptionsLibrary.FORBIDDEN);
-        (
-            ,
-            ,
-            address token0,
-            address token1,
-            uint24 fee,
-            int24 tickLower,
-            int24 tickUpper,
-            uint128 liquidity,
-            uint256 feeGrowthInside0LastX128,
-            uint256 feeGrowthInside1LastX128,
-            uint128 tokensOwed0,
-            uint128 tokensOwed1
-        ) = positionManager.positions(nft);
-        IUniswapV3Pool pool = factory.getPool(token0, token1, fee);
-        require(protocolGovernance.isPoolWhitelisted(address(pool)), ExceptionsLibrary.INVALID_POOL);
+        if (isPrivate && !_depositorsAllowlist.contains(msg.sender)) {
+            revert ExceptionsLibrary.AllowList();
+        }
 
-        positionManager.transferFrom(msg.sender, address(this), nft);
+        _requireVaultOwner(vaultId);
 
-        PositionInfo memory position = PositionInfo({
-            token0: token0,
-            token1: token1,
-            fee: fee,
-            positionKey: keccak256(abi.encodePacked(address(positionManager), tickLower, tickUpper)),
-            liquidity: liquidity,
-            feeGrowthInside0LastX128: feeGrowthInside0LastX128,
-            feeGrowthInside1LastX128: feeGrowthInside1LastX128,
-            tokensOwed0: tokensOwed0,
-            tokensOwed1: tokensOwed1,
-            sqrtPriceAX96: TickMath.getSqrtRatioAtTick(tickLower),
-            sqrtPriceBX96: TickMath.getSqrtRatioAtTick(tickUpper),
-            targetPool: pool,
-            vaultId: vaultId
-        });
+        {
+            (
+                ,
+                ,
+                address token0,
+                address token1,
+                uint24 fee,
+                int24 tickLower,
+                int24 tickUpper,
+                uint128 liquidity,
+                uint256 feeGrowthInside0LastX128,
+                uint256 feeGrowthInside1LastX128,
+                uint128 tokensOwed0,
+                uint128 tokensOwed1
+            ) = positionManager.positions(nft);
+            IUniswapV3Pool pool = IUniswapV3Pool(factory.getPool(token0, token1, fee));
+
+            if (!protocolGovernance.isPoolWhitelisted(address(pool))) {
+                revert ExceptionsLibrary.InvalidPool();
+            }
+
+            positionManager.transferFrom(msg.sender, address(this), nft);
+
+            PositionInfo memory position = PositionInfo({
+                token0: token0,
+                token1: token1,
+                fee: fee,
+                positionKey: keccak256(abi.encodePacked(address(positionManager), tickLower, tickUpper)),
+                liquidity: liquidity,
+                feeGrowthInside0LastX128: feeGrowthInside0LastX128,
+                feeGrowthInside1LastX128: feeGrowthInside1LastX128,
+                tokensOwed0: tokensOwed0,
+                tokensOwed1: tokensOwed1,
+                sqrtPriceAX96: TickMath.getSqrtRatioAtTick(tickLower),
+                sqrtPriceBX96: TickMath.getSqrtRatioAtTick(tickUpper),
+                targetPool: pool,
+                vaultId: vaultId
+            });
+
+            _positionInfo[nft] = position;
+        }
 
         // todo: check limits
 
-        vaultNfts[vaultId].add(nft);
+        _vaultNfts[vaultId].add(nft);
     }
 
     function withdrawCollateral(uint256 nft) external {
         PositionInfo memory position = _positionInfo[nft];
-        require(vaultOwner[vaultId] == msg.sender, ExceptionsLibrary.FORBIDDEN);
+        _requireVaultOwner(position.vaultId);
 
-        uint256 liquidationThreshold = protocolGovernance.liquidationThreshold(position.targetPool);
-        uint256 result = calculateHealthFactor(position.vaultId) - _calculatePosition(nft, liquidationThreshold);
+        uint256 liquidationThreshold = protocolGovernance.liquidationThreshold(address(position.targetPool));
+        uint256 result = calculateHealthFactor(position.vaultId) - _calculatePosition(position, liquidationThreshold);
 
         // checking that health factor is more or equal than 1
-        require(result >= debt[position.vaultId], ExceptionsLibrary.LIMIT_UNDERFLOW);
+        if (result < debt[position.vaultId]) {
+            revert ExceptionsLibrary.PositionUnhealthy();
+        }
 
         positionManager.safeTransferFrom(address(this), msg.sender, nft);
 
-        vaultNfts.remove(nft);
+        _vaultNfts[position.vaultId].remove(nft);
         delete _positionInfo[nft];
+    }
+
+    function _updateDebt(uint256 vaultId) internal {
+
     }
 
     function calculateHealthFactor(uint256 vaultId) public view returns (uint256) {
         uint256 result = 0;
-        for (uint256 i = 0; i < vaultNfts[vaultId].length(); ++i) {
-            uint256 nft = vaultNfts[vaultId].at(i);
-            uint256 liquidationThreshold = protocolGovernance.liquidationThreshold(position.targetPool);
+        for (uint256 i = 0; i < _vaultNfts[vaultId].length(); ++i) {
+            uint256 nft = _vaultNfts[vaultId].at(i);
+            uint256 liquidationThreshold = protocolGovernance.liquidationThreshold(address(_positionInfo[nft].targetPool));
             result += _calculatePosition(_positionInfo[nft], liquidationThreshold);
         }
         return result;
@@ -182,8 +231,8 @@ contract Vault is DefaultAccessControl {
 
     function _calculateVaultAmount(uint256 vaultId) internal view returns (uint256) {
         uint256 result = 0;
-        for (uint256 i = 0; i < vaultNfts[vaultId].length(); ++i) {
-            uint256 nft = vaultNfts[vaultId].at(i);
+        for (uint256 i = 0; i < _vaultNfts[vaultId].length(); ++i) {
+            uint256 nft = _vaultNfts[vaultId].at(i);
             result += _calculatePosition(_positionInfo[nft], DENOMINATOR);
         }
         return result;
@@ -194,7 +243,7 @@ contract Vault is DefaultAccessControl {
         view
         returns (uint256)
     {
-        tokenAmounts = new uint256[](2);
+        uint256[] memory tokenAmounts = new uint256[](2);
         (uint160 sqrtPriceX96, , , , , , ) = position.targetPool.slot0();
 
         (tokenAmounts[0], tokenAmounts[1]) = LiquidityAmounts.getAmountsForLiquidity(
@@ -228,7 +277,7 @@ contract Vault is DefaultAccessControl {
                 )
             );
 
-        uint256 pricesX96 = new uint256[](2);
+        uint256[] memory pricesX96 = new uint256[](2);
         pricesX96[0] = oracle.price(position.token0);
         pricesX96[1] = oracle.price(position.token1);
 
@@ -242,23 +291,33 @@ contract Vault is DefaultAccessControl {
     }
 
     function mintDebt(uint256 vaultId, uint256 amount) external {
-        require(vaultOwner[vaultId] == msg.sender, ExceptionsLibrary.FORBIDDEN);
+        _requireVaultOwner(vaultId);
         uint256 healthFactor = calculateHealthFactor(vaultId);
-        require(healthFactor > debt[vaultId] + amount, ExceptionsLibrary.LIMIT_OVERFLOW);
+
+        if (healthFactor < debt[vaultId] + amount) {
+            revert ExceptionsLibrary.PositionUnhealthy();
+        }
+
         token.mint(msg.sender, amount);
         debt[vaultId] += amount;
     }
 
     function burnDebt(uint256 vaultId, uint256 amount) external {
-        require(vaultOwner[vaultId] == msg.sender, ExceptionsLibrary.FORBIDDEN);
-        require(debt[vaultId] >= amount, ExceptionsLibrary.LIMIT_OVERFLOW);
+        _requireVaultOwner(vaultId);
+
+        if (amount > debt[vaultId]) {
+            revert ExceptionsLibrary.DebtOverflow();
+        }
+
         token.burn(msg.sender, amount);
         debt[vaultId] -= amount;
     }
 
     function liquidate(uint256 vaultId) external {
         uint256 healthFactor = calculateHealthFactor(vaultId);
-        require(healthFactor < debt[vaultId], ExceptionsLibrary.LIMIT_UNDERFLOW);
+        if (healthFactor >= debt[vaultId]) {
+            revert ExceptionsLibrary.PositionHealthy();
+        }
 
         uint256 vaultAmount = _calculateVaultAmount(vaultId);
         uint256 returnAmount = FullMath.mulDiv(
@@ -268,11 +327,17 @@ contract Vault is DefaultAccessControl {
         );
         token.transferFrom(msg.sender, address(this), returnAmount);
 
-        uint256 daoReceiveAmount = FullMath.mulDiv(protocolGovernance.protocolParams().liquidationFee);
+        uint256 daoReceiveAmount = FullMath.mulDiv(protocolGovernance.protocolParams().liquidationFee, vaultAmount, DENOMINATOR);
         token.transfer(treasury, daoReceiveAmount);
-        token.transfer(vaultOwners[i], returnAmount - daoRecieveAmount - debt[vaultId]);
+        token.transfer(vaultOwners[vaultId], returnAmount - daoReceiveAmount - debt[vaultId]);
 
         _closeVault(vaultId, vaultOwners[vaultId], msg.sender);
+    }
+
+    function _requireVaultOwner(uint256 vaultId) internal view {
+        if (vaultOwners[vaultId] != msg.sender) {
+            revert ExceptionsLibrary.Forbidden();
+        }
     }
 
     function pause() external {
--- src/libraries/ExceptionsLibrary.sol
+++ src/libraries/ExceptionsLibrary.sol
@@ -4,11 +4,17 @@ pragma solidity 0.8.13;
 /// @notice Errors stores project`s smart-contracts errors
 library ExceptionsLibrary {
     error AddressZero();
+    error AllowList();
+    error DebtOverflow();
     error Duplicate();
     error Forbidden();
-    error InvalidValue();
     error InvalidLength(uint256 actualLength, uint256 targetLength);
+    error InvalidPool();
+    error InvalidValue();
     error Null();
+    error PositionHealthy();
+    error PositionUnhealthy();
     error Timestamp();
+    error UnpaidDebt();
     error ValueZero();
 }
