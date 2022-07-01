525d904b29081bedb2ebc4e5887ef8b44e32dc21
cube <cubetv228@gmail.com>
2022-07-01 17:41:53 +0300

    added comments to MUSD

--- src/MUSD.sol
+++ src/MUSD.sol
@@ -4,9 +4,16 @@ pragma solidity ^0.8.13;
 import "@solmate/src/tokens/ERC20.sol";
 
 contract MUSD is ERC20 {
+    /// @notice Thrown when a user has not permissions to perform a certain action.
     error Forbidden();
+
+    /// @notice The only vault, which is allowed to mint or burn MUSD (remains constant after contract creation).
     address public immutable governingVault;
 
+    /// @notice Creates a new contract.
+    /// @param name ERC20 token name
+    /// @param symbol ERC20 token symbol
+    /// @param vault Address of the governing vault
     constructor(
         string memory name,
         string memory symbol,
@@ -15,6 +22,11 @@ contract MUSD is ERC20 {
         governingVault = vault;
     }
 
+    // -------------------  EXTERNAL, MUTATING  -------------------
+
+    /// @notice Mint an amount of MUSD for a specified address.
+    /// @param to Address of the receiver of MUSD
+    /// @param amount Amount of MUSD to mint
     function mint(address to, uint256 amount) external {
         if (msg.sender != governingVault) {
             revert Forbidden();
@@ -22,6 +34,9 @@ contract MUSD is ERC20 {
         _mint(to, amount);
     }
 
+    /// @notice Burn an amount of MUSD of a specified address.
+    /// @param from Address of the holder of MUSD
+    /// @param amount Amount of MUSD to burn
     function burn(address from, uint256 amount) external {
         if (msg.sender != governingVault) {
             revert Forbidden();
