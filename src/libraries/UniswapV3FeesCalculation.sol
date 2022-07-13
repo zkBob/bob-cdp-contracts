8cd715ea12c5e27a86c8318eb7792875e4aa933d
cube <cubetv228@gmail.com>
2022-07-13 15:49:57 +0300

    added some comments to UniV3FeesCalculation

--- src/libraries/UniswapV3FeesCalculation.sol
+++ src/libraries/UniswapV3FeesCalculation.sol
@@ -8,6 +8,14 @@ import "../interfaces/external/univ3/IUniswapV3Pool.sol";
 library UniswapV3FeesCalculation {
     uint256 public constant Q128 = 2**128;
 
+    /// @notice Additional information about UniV3 position
+    /// @param tickLower The lower tick boundary of the position
+    /// @param tickUpper The upper tick boundary of the position
+    /// @param liquidity The amount of liquidity owned by this position
+    /// @param feeGrowthInside0LastX128 Fee growth of token0 inside the tick range as of the moment of the deposit
+    /// @param feeGrowthInside1LastX128 Fee growth of token1 inside the tick range as of the moment of the deposit
+    /// @param tokensOwed0 The computed amount of token0 owed to the position as of the moment of the deposit
+    /// @param tokensOwed1 The computed amount of token1 owed to the position as of the moment of the deposit
     struct PositionInfo {
         int24 tickLower;
         int24 tickUpper;
