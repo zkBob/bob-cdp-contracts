72508229cad67b54ab4d9e55c87e0c0e9dd4c872
Alexey Saplin <40490358+AlexSaplin@users.noreply.github.com>
2023-01-05 18:40:23 +0700

    minor fix

--- test/shared/Env.sol
+++ test/shared/Env.sol
@@ -4,5 +4,5 @@ pragma solidity 0.8.15;
 
 uint256 constant forkBlockMainnet = 16200000;
 string constant forkRpcUrlMainnet = "https://rpc.ankr.com/eth";
-uint256 constant forkBlockPolygon = 37000000;
+uint256 constant forkBlockPolygon = 37610791;
 string constant forkRpcUrlPolygon = "https://rpc.ankr.com/polygon";
