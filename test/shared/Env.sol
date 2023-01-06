219947ce7b986e29f0fa9b3eeee771bd8f60e9a1
Alexey Saplin <40490358+AlexSaplin@users.noreply.github.com>
2023-01-06 10:49:56 +0700

    solidity versions fix

--- test/shared/Env.sol
+++ test/shared/Env.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: CC0-1.0
 
-pragma solidity 0.8.15;
+pragma solidity ^0.8.0;
 
 uint256 constant forkBlockMainnet = 16200000;
 string constant forkRpcUrlMainnet = "https://rpc.ankr.com/eth";
--- test/shared/ForkTests.sol
+++ test/shared/ForkTests.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: CC0-1.0
 
-pragma solidity 0.8.15;
+pragma solidity ^0.8.0;
 
 import "forge-std/Test.sol";
 import "./Env.sol";
