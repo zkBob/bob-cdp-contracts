e26e1917bf6602009cc2d3247a8607a53a0253ac
Artyom Yusupov <iusupov.ai@phystech.edu>
2023-01-26 11:05:38 +0400

    goerli deployment configs

---
+++ src/script/uniswap/UniswapGoerliDeployment.s.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "./AbstractUniswapDeployment.sol";
+
+contract UniswapGoerliDeployment is AbstractUniswapDeployment, AbstractGoerliDeployment {}
