54e4046855194fb331a73729c0505b335c60473f
Artyom Yusupov <iusupov.ai@phystech.edu>
2023-02-10 12:03:03 +0400

    fname changed

--- test/NftOracleQuickswap.t.sol
+++ test/NftOracleQuickswap.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "./AbstractNftOracle.sol";
+import "./AbstractNftOracle.t.sol";
 import "./shared/AbstractQuickswapHelper.sol";
 
 contract NftOracleQuickswapTest is
--- test/NftOracleUniswap.t.sol
+++ test/NftOracleUniswap.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "./AbstractNftOracle.sol";
+import "./AbstractNftOracle.t.sol";
 import "./shared/AbstractUniswapHelper.sol";
 
 contract NftOracleUniswapTest is
